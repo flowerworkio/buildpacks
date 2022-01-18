4a1d101099a764e0e85a1e2f19668f12fad9185e
Patrick Wiseman <patrick@flowerwork.io>
2022-01-18 10:07:28 -0500

    Start of phoenix buildpack

---
+++ phoenix/bin/io.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+function io::title() {
+  local blue reset message
+  blue="\033[0;34m"
+  reset="\033[0;39m"
+  message="${1}"
+
+  echo -e "\n${blue}${message}${reset}" >&2
+}
+
+function io::info() {
+  local message
+  message="${1}"
+
+  echo -e "${message}" >&2
+}
+
+function io::error() {
+  local message red reset
+  message="${1}"
+  red="\033[0;31m"
+  reset="\033[0;39m"
+
+  echo -e "${red}${message}${reset}" >&2
+  exit 1
+}
+
+function io::success() {
+  local message green reset
+  message="${1}"
+  green="\033[0;32m"
+  reset="\033[0;39m"
+
+  echo -e "${green}${message}${reset}" >&2
+  exitcode="${2:-0}"
+  exit "${exitcode}"
+}
+
+function io::warn() {
+  local message yellow reset
+  message="${1}"
+  yellow="\033[0;33m"
+  reset="\033[0;39m"
+
+  echo -e "${yellow}${message}${reset}" >&2
+  exit 0
+}
