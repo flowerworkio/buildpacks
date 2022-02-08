47bfc447f5b605592f02fc27e50d7cf1c3148a56
Patrick Wiseman <patrick@flowerwork.io>
2022-02-08 17:42:23 -0500

    Move buildpacks CI configuration to source control * Pre-command will add homebrew packages to the path * Added functions for packaging and publishing the stack images, builder   image, and buildpack images * Package steps configured to run on all branches * Publish steps configured to only run on main branch

---
+++ bin/functions.sh
@@ -0,0 +1,45 @@
+brew_add_to_shell() {
+  describe "Add homebrew to shell"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+}
+
+describe() {
+  echo "--- $1…"
+}
+
+package_stacks() {
+  describe "Packaging stacks"
+  docker build ./stacks/focal -t flowerworkio/base:focal --target base
+  docker build ./stacks/focal -t flowerworkio/run:focal --target run
+  docker build ./stacks/focal -t flowerworkio/build:focal --target build
+}
+
+publish_stacks(){
+  describe "Publishing stacks"
+  docker push flowerworkio/build:focal
+  docker push flowerworkio/run:focal
+}
+
+package_builder(){
+  describe "Packaging builder"
+  pack builder create flowerworkio/builder:focal --config ./builder.toml
+}
+
+publish_builder(){
+  describe "Publishing builder"
+  docker push flowerworkio/builder:focal
+}
+
+package_buildpacks(){
+  describe "Packaging buildpacks"
+  flowerworkio_asdf_version=$(cat asdf/buildpack.toml | yj -tj | jq -r '.buildpack.version')
+  flowerworkio_phoenix_version=$(cat phoenix/buildpack.toml | yj -tj | jq -r '.buildpack.version')
+  pack buildpack package flowerworkio/asdf:$flowerworkio_asdf_version --config asdf/package.toml
+  pack buildpack package flowerworkio/phoenix:$flowerworkio_phoenix_version --config phoenix/package.toml
+}
+
+publish_buildpacks(){
+  describe "Publishing buildpacks"
+  docker push flowerworkio/asdf:$flowerworkio_asdf_version
+  docker push flowerworkio/phoenix:$flowerworkio_phoenix_version
+}
